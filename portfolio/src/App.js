cbb147f86891b763dd476e1e00ff2f64640b8d62
Rivenf <39199060+rivenf@users.noreply.github.com>
2024-10-14 15:10:01 +0000

    Add style

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header.js";
+import About from "./About.js";
+import Gallery from "./Gallery.js";
+import Footer from "./Footer.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Header />
+      <About />
+      <Gallery />
+      <Footer />
+    </main>
   );
 }
 
