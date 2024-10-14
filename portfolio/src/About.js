85670e6fe7c41cadf1cc104a94bf2dd8392919ca
Rivenf <39199060+rivenf@users.noreply.github.com>
2024-10-14 15:09:46 +0000

    create component structure

---
+++ portfolio/src/About.js
@@ -0,0 +1,22 @@
+export default function About() {
+  return (
+    <div className="container aboutGrid">
+      <div>
+        <h2>About Me</h2>
+        <p>
+          Hello, I'm Nico! I am a front-end developer with a background in art,
+          photography, and design. I have spent the last several years working
+          in technology education, and I am looking to get back to my design and
+          development roots.
+        </p>
+        <p>
+          I am based in Maryland, and in my spare time I enjoy getting outside
+          and spending time in nature. When the weather refuses to cooperate, I
+          like to spend time indoors crafting, reading, or learning new
+          programming skills.
+        </p>
+      </div>
+      <img src="images/Nico.jpg" alt="Nico Rivera" className="aboutImg" />
+    </div>
+  );
+}
---
+++ portfolio/src/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="socialIcons"></div>
+      <p>Copyright © 2022 Nico Rivera</p>
+    </div>
+  );
+}
