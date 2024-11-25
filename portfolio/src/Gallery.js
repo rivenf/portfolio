92fdb10b13321199bf22632e4cf9ecd5304431a4
Rivenf <39199060+rivenf@users.noreply.github.com>
2024-11-25 13:12:24 +0000

    Build gallery overlay popup

--- portfolio/src/Gallery.js
+++ portfolio/src/Gallery.js
@@ -1,15 +1,38 @@
 import projectData from "./projectData.json";
+import { useState } from "react";
 
 function ProjectCard({ project }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="galleryCard">
-      <img src={project.thumbnail} alt={project.title} className="galleryImg" />
-      <h3>{project.title}</h3>
-      <p>{project.description}</p>
-      <a href={project.link} target="_blank">
-        Link to GitHub
-      </a>
-    </div>
+    <>
+      <div className="galleryCard">
+        <img
+          src={project.thumbnail}
+          alt={project.title}
+          className="galleryImg"
+        />
+        <h3>{project.title}</h3>
+        <p>{project.description}</p>
+        <a href={project.link} target="_blank">
+          Link to GitHub
+        </a>
+        <button className="galleryBtn" onClick={() => setIsOpen(true)}>
+          View details
+        </button>
+      </div>
+      {isOpen ? (
+        <div className="overlay">
+          <div className="overlayContainer">
+            <p>Pictures go here - todo later</p>
+            <h3>{project.title}</h3>
+            <button className="galleryBtn" onClick={() => setIsOpen(false)}>
+              Close
+            </button>
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 }
 
